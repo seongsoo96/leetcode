47e70756efbb359c70fb51d50938a43d392146e6
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-11-13 12:58:22 +0900

    Time: 102 ms (88.08%), Space: 49.8 MB (16.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -15,7 +15,7 @@ var reverseString = function(s) {
     let left = 0;
     let right = s.length-1;
     while(left <= right) {
-        [s[right], s[left]] = [s[left], s[right]]
+        [s[left], s[right]] = [s[right], s[left]]
         left++;
         right--;
     }
