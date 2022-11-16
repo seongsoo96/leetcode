ce8b362ad17a7d1f3ac04aef8865994d89097fef
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-11-16 11:32:32 +0900

    Time: 125 ms (60.07%), Space: 44.3 MB (78.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -2,20 +2,38 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function(nums) {
-    for(let i=0; i<nums.length; i++) {
-        let leftSum = 0;
-        let rightSum = 0;
-        for(let j=0; j<i; j++) {
-            leftSum += nums[j];
-        }
-        for(let k=i+1; k<nums.length; k++) {
-            rightSum += nums[k];
-        }
+
+// My Solution
+// var pivotIndex = function(nums) {
+//     for(let i=0; i<nums.length; i++) {
+//         let leftSum = 0;
+//         let rightSum = 0;
+//         for(let j=0; j<i; j++) {
+//             leftSum += nums[j];
+//         }
+//         for(let k=i+1; k<nums.length; k++) {
+//             rightSum += nums[k];
+//         }
         
-        if(leftSum === rightSum) {
+//         if(leftSum === rightSum) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// };
+
+// Better Solution
+var pivotIndex = function(nums) {
+    let sum = 0
+    let leftsum = 0;
+    for (const x of nums) {
+        sum += x;
+    } 
+    for (let i = 0; i < nums.length; i++) {
+        if (leftsum === sum - leftsum - nums[i]) {
             return i;
         }
+        leftsum += nums[i];
     }
     return -1;
 };
