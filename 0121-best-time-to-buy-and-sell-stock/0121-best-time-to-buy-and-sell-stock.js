ea8bcc7f8e77aba510402db8312f1967f96ce65e
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-11-26 12:20:09 +0900

    Time: 87 ms (94.82%), Space: 52.3 MB (8.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,6 +2,24 @@
  * @param {number[]} prices
  * @return {number}
  */
+//첫번째 풀이
+var maxProfit = function(prices) {
+  let left = 0;
+  let right = 0;
+  let max_profit = 0;
+  while(right < prices.length) {
+    if(prices[left] < prices[right]) {
+      let current_profit = prices[right] - prices[left];
+      max_profit = max_profit > current_profit ? max_profit : current_profit;
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit; 
+};
+
+//두번째 풀이
 var maxProfit = function(prices) {
     let min = Number.MAX_SAFE_INTEGER
     let maxProfit = 0
