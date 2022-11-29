602b6289933b329166d50a3e152d9909535055d5
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-11-29 22:30:22 +0900

    Time: 94 ms, Memory: 44.4 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -5,18 +5,10 @@
 var findNumbers = function(nums) {
     let cnt = 0;
     for(let n of nums) {
-        // let digitsCnt = 1;
         n = n+'';
         if(n.length % 2 === 0) {
             cnt++;
         }
-        // while(n >= 10) {
-        //     n = n/10;
-        //     digitsCnt++;
-        // }
-        // if(digitsCnt%2 === 0) {
-        //     cnt++;
-        // }
     }
     return cnt;
 };
