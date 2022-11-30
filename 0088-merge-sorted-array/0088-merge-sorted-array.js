930278f758312053956bc9626a2ffd3d19d5a506
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-11-30 15:39:07 +0900

    Time: 119 ms (10.15%), Space: 42 MB (61.80%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,6 +5,7 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+// My solution
 var merge = function(nums1, m, nums2, n) {
     if(nums1.length > m) {
         while(nums1.length !== m) {
@@ -15,4 +16,13 @@ var merge = function(nums1, m, nums2, n) {
         nums1.push(nums2[i]);
     }
     nums1.sort((a, b) => a-b);
+};
+
+// Other solution
+var merge = function (nums1, m, nums2, n) {
+    var insertPos = m + n - 1;
+    m--; n--;
+    while (n >= 0) {
+        nums1[insertPos--] = (nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--];
+    }
 };
