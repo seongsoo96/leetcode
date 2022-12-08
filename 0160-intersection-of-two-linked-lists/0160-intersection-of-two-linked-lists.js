17f66d95185910d682716852feaeb832940b9815
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-08 16:47:59 +0900

    Time: 174 ms (20.46%), Space: 50 MB (61.28%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+//Approach 1 : Brute Force 
+var getIntersectionNode = function(headA, headB) {
+    while(headA != null) {
+        let bHead = headB;
+        while(bHead != null) {
+            if(headA === bHead) {
+                return headA
+            }
+            bHead = bHead.next;
+        }
+        headA = headA.next;
+    }
+    return null
+};
+
+//Approach 2 : Two Pointers
+var getIntersectionNode = function(headA, headB) {
+    let pA = headA;
+    let pB = headB;
+    while(pA != pB) {
+        pA = pA ? pA.next : headB
+        pB = pB ? pB.next : headA
+    }
+    return pA;
+};
