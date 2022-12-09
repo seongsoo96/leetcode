d64abd25272ed7b6f651925d62cfbe8f3c50a8fb
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-09 16:10:33 +0900

    Time: 165 ms, Memory: 47 MB - LeetHub

---
+++ move-zeroes/move-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let index = 0;
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] !== 0) {
+            [nums[index], nums[i]] = [nums[i], nums[index]];
+            index++
+        }
+    }
+    return nums;
+};
