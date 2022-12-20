26c43e5bffba8fff30e5e687ebfd3b53eccbd43b
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-20 10:12:22 +0900

    Time: 210 ms, Memory: 47.3 MB - LeetHub

---
+++ array-partition-i/array-partition-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    let sort = nums.sort((a,b) => b-a);
+    let sum = 0;
+    for(let i=0; i<nums.length; i++) {
+        if(i%2 === 1) {
+            sum += nums[i];
+        }
+    }
+    return sum
+};
