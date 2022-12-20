5b8cd9485ee08f78631626cf63a69939847e9fb7
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-20 10:22:32 +0900

    Time: 565 ms, Memory: 43 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    for(let i=0; i<numbers.length; i++) {
+        for(let j=i+1; j<numbers.length; j++) {
+            if(target === numbers[i] + numbers[j]) {
+                return [i+1, j+1]
+            }
+        }
+    }
+};
