182dbda195003e738f22203036ce446fc1be5dc7
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-20 09:59:14 +0900

    Time: 175 ms, Memory: 49.9 MB - LeetHub

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse();
+    let [left, right] = [0, s.length-1];
+    for(let i=0; i<s.length/2; i++) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+
+    return s;
 };
