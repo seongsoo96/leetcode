0f5145c072e46139a318516cb9cadaf0d751ec2b
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-27 11:38:17 +0900

    Time: 383 ms (35.51%), Space: 64.9 MB (41.12%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 var maximumSum = function(nums) {
-    const map = new Map()
-    for(const n of nums) {    
-        let sum = (n + '').split('').map(n => Number(n)).reduce((a,c) => a+c)
-        if(!map.has(sum)) {
-            map.set(sum, [])
-        }
-        map.get(sum).push(n)
-    }
-    
+    const dic = new Map()
     let ans = -1;
-    for(const [key, arr] of map) {
-        if(arr.length > 1) {
-            arr.sort((a,b) => b-a)
-            ans = Math.max(ans, arr[0] + arr[1])
+    for (const num of nums) {
+        let digitSum = (num + '').split('').map(num => Number(num)).reduce((a,c) => a+c)
+        if (dic.has(digitSum)) {
+            ans = Math.max(ans, num + dic.get(digitSum));
         }
+        
+        dic.set(digitSum, Math.max(dic.get(digitSum) || 0, num));
     }
     
     return ans;
