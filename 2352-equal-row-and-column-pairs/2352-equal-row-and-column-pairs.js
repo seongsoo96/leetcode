e1b0b83eb504cabb1589eb0b0214e8e7cc65bfa6
Seongsoo Lee <seongsoo.dev@gmail.com>
2022-12-28 14:48:44 +0900

    Time: 194 ms (72.29%), Space: 54.9 MB (56.63%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} gird
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+    const row = [];
+    for(let i=0; i<grid.length; i++) {
+        row[i] = [];
+        for(let j=0; j<grid[0].length; j++) {
+            row[i][j] = grid[j][i];
+        }
+    }
+    for(let i=0; i<grid.length; i++) {
+        grid[i] = grid[i].toString();
+        row[i] = row[i].toString();
+    }
+    let count = 0;
+    const map = new Map()
+    for(const g of grid) {
+        if(!map.has(g)) {
+            map.set(g, 0);
+        }
+        map.set(g, map.get(g)+1);
+    }
+    console.log(map);
+    for(const r of row) {
+        if(map.has(r)) {
+            count += map.get(r);
+        }
+    }
+    
+    
+    
+    console.log(grid);
+    console.log(row);
+    return count;
+};
