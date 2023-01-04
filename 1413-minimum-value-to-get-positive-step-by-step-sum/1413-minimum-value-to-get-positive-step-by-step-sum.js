562ca825f189634585f6306130ed00b9ea6d4dda
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-01-04 09:59:22 +0900

    Time: 84 ms (48.38%), Space: 42.3 MB (23.61%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -6,9 +6,7 @@ var minStartValue = function(nums) {
     for(let i=1; i<nums.length; i++) {
         nums[i] += nums[i-1];
     }
-    console.log(nums);
     nums.sort((a,b) => a-b);
-    console.log(nums);
     if(nums[0] > 0) {
         return 1;
     } else {
