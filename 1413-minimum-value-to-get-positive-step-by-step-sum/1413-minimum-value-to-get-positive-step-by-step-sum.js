cc7abce833ba2a8d002462489ddfb7e15e672b69
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-01-04 10:07:04 +0900

    Time: 128 ms (5.09%), Space: 42.1 MB (32.64%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -3,14 +3,10 @@
  * @return {number}
  */
 var minStartValue = function(nums) {
-    for(let i=1; i<nums.length; i++) {
-        nums[i] += nums[i-1];
+    let min = 0, total = 0;
+    for(const num of nums) {
+        total += num;
+        min = Math.min(min,  total);
     }
-    let min = Number.MAX_SAFE_INTEGER
-    for(let i=0; i<nums.length; i++) {
-        if(nums[i] < min) {
-            min = nums[i]
-        }
-    }
-    return min >= 0 ? 1 : 1-min
+    return 1-min
 };
