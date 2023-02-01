2e2ea4e2ab94e445c3bafd9d69be32342d079372
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-02-01 10:11:21 +0900

    Time: 68 ms (62.46%), Space: 42.1 MB (51.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,21 +4,25 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    for(const nums of matrix) {
-        let left = 0
-        let right = nums.length - 1
-        while(left <= right) {
-            let mid = Math.floor((left + right) / 2)
-            let num = nums[mid]
-            if(num === target) {
-                return true
-            }
-            if(num > target) {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
+    let m = matrix.length, n = matrix[0].length;
+    let left = 0, right = m * n - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let row = Math.floor(mid / n);
+        let col = mid % n;
+        let num = matrix[row][col];
+        
+        if (num == target) {
+            return true;
+        }
+        
+        if (num < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    return false
+    
+    return false;
 };
