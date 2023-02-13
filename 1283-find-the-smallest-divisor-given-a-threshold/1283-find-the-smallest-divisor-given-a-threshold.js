9e0444293a9789210e58b5b9531fbff8ad31933e
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-02-13 11:28:12 +0900

    Time: 122 ms (24.39%), Space: 44.5 MB (78.05%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
@@ -4,38 +4,17 @@
  * @return {number}
  */
 // Return the sum of division results with 'divisor'.
-let findDivisionSum = (nums, divisor) => {
-    let result = 0;
-    for (let index in nums) {
-        const num = nums[index];
-        result += Math.ceil((1.0 * num) / divisor);
-    }
-    return result;
-}
-
-let smallestDivisor = function(nums, threshold) {
-    let ans = -1;
+var smallestDivisor = function(nums, threshold) {
+    let left = 0;
+    let right = 1e6;
+    
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        const sum = nums.reduce((acc, num) => acc + Math.ceil(num / mid), 0);
         
-    let low = 1;
-    let high = nums.reduce((a, b) => Math.max(a, b), nums[0]);
-
-    // Iterate using binary search on all divisors.
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        const result = findDivisionSum(nums, mid);
-        // If current divisor does not exceed threshold, 
-        // then it can be our answer, but also try smaller divisors
-        // thus change search space to left half.
-        if (result <= threshold) {
-            ans = mid;
-            high = mid - 1;
-        }
-        // Otherwise, we need a bigger divisor to reduce the result sum
-        // thus change search space to right half.
-        else {
-            low = mid + 1;
-        }
+        if (threshold < sum) left = mid + 1;
+        else right = mid;
     }
-
-    return ans;
+    
+    return left;
 };
