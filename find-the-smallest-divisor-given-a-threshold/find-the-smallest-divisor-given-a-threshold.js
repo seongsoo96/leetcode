7992089a9937d79fa4cbc391de44e71971ac7d06
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-02-13 14:09:55 +0900

    Time: 113 ms, Memory: 44.7 MB - LeetHub

---
+++ find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    let ans = -1
+
+    let low = 0
+    let high = nums.reduce((acc, num) => Math.max(acc, num), 0)
+
+    while(low <= high) {
+       const mid = Math.floor((low + high) / 2)
+       const result = nums.reduce((acc, num) => acc + Math.ceil(num / mid), 0)
+
+       if(result <= threshold) {
+           ans = mid
+           high = mid - 1
+       } else {
+           low = mid + 1
+       }
+    }
+    return ans
+};
