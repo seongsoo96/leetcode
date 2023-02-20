3a25b8bac396cbb704ba863c4f2ff224e34d50c7
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-02-20 16:32:58 +0900

    Time: 72 ms (28.38%), Space: 42.1 MB (55.06%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,18 +4,21 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1
-    while(left <= right) {
-        let mid = Math.floor((left+right) / 2)
+    let small = 0
+    let big = nums.length - 1
+    
+    while(small <= big) {
+        let mid = Math.floor((small + big) / 2)
+        
         if(nums[mid] === target) {
             return mid
         }
+        
         if(nums[mid] > target) {
-            right = mid - 1
+            big = mid -1
         } else {
-            left = mid + 1
+            small = mid + 1
         }
     }
-    return left
+    return small
 };
