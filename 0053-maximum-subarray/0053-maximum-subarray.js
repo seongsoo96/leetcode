1596e7541d4ab1128a2a7db440e7e04130e7abee
Seongsoo Lee <seongsoo.dev@gmail.com>
2023-03-03 11:11:19 +0900

    Time: 94 ms (37.06%), Space: 50.2 MB (73.31%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -11,13 +11,4 @@ var maxSubArray = function(nums) {
     }
     
     return max_subarray
-    
-//     var prev = 0;
-//     var max = -Number.MAX_VALUE;
-
-//     for (var i = 0; i < A.length; i++) {
-//     prev = Math.max(prev + A[i], A[i]);
-//     max = Math.max(max, prev);
-//     }
-//     return max;
 };
